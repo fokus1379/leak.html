f5676d53476d2b45b6644a1137f1a19167eae80f
fokus1379 <150222992+fokus1379@users.noreply.github.com>
2023-11-08 16:53:21 +0700

    Add files via upload

---
+++ royal.js
@@ -0,0 +1,16 @@
+{
+  "items": [
+    {
+      "url": "https://files.sitestatic.net/banners/pU70ClkKhprAzk9tGpoyCEEu2WODhL6aF8hJYl7R.jpg",
+      "alt": "Welcome bonus slot 100%"
+    },
+    {
+      "url": "https://files.sitestatic.net/banners/cYptDurg0kgSV389RXbqsTnnJNnvCB11P4WdYUCl.jpg",
+      "alt": "komisi dibagikan setiap hari"
+    },
+    {
+      "url": "https://files.sitestatic.net/banners/y5g2MbpLXONkWZHTWDE9DT3R6azhEyFuWBzv0MiI.jpg",
+      "alt": "menerima segala jenis deposit"
+    }
+  ]
+}
